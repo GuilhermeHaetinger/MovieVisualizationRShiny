8e7f2b6d9a1a4b495425dd9e50a8e15453f885a2
Guilherme Haetinger <ghaetinger@gmail.com>
2020-09-08 17:17:41 -0300

    added shiny structure

--- server.R
+++ server.R
@@ -433,29 +433,3 @@ server <- function(input, output) {
     }
   })
 }
-
-ui <- fluidPage(
-  theme=shinytheme('superhero'),
-  headerPanel("Movie DB Analisys!"),
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("threshold", label = "threshold",
-                  min = 0, max = 10, value = 1, step = 1),
-      selectInput("df", "Movie Database", c("Popular Movies", "Top Rated Movies"),
-                  selected = "Popular Movies"),
-      checkboxInput("sankey", "Sankey", value=TRUE),
-        fluidRow(
-          column(6,
-                 uiOutput("frame")),
-          column(5,
-                 offset = 1,
-                 htmlOutput("description"))
-      ),
-    ),
-    mainPanel(
-      plotlyOutput('plot')
-    )
-  )
-)
-app <- shinyApp(ui = ui, server = server)
-runApp(app)##, port=5533)
---
+++ ui.R
@@ -0,0 +1,24 @@
+library(shiny)
+ui <- fluidPage(
+  theme=shinytheme('superhero'),
+  headerPanel("Movie DB Analisys!"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("threshold", label = "threshold",
+                  min = 0, max = 10, value = 1, step = 1),
+      selectInput("df", "Movie Database", c("Popular Movies", "Top Rated Movies"),
+                  selected = "Popular Movies"),
+      checkboxInput("sankey", "Sankey", value=TRUE),
+        fluidRow(
+          column(6,
+                 uiOutput("frame")),
+          column(5,
+                 offset = 1,
+                 htmlOutput("description"))
+      ),
+    ),
+    mainPanel(
+      plotlyOutput('plot')
+    )
+  )
+)
